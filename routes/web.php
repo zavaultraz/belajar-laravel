42c403208807d7924c92f555ff7a55443c134425
zavaultraz <zavazaneta@gmail.com>
2024-02-16 09:49:57 +0700

    belajar

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutControlle;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfilController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// route for admincontroller with rsc
+Route::resource('admin', AdminController::class);
+
+
+
